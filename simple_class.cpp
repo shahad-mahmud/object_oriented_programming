8aec0d5e051d7d3e45613ba32ac04322b4653edc
shahad-mahmud <shahad9381@gmail.com>
2020-05-28 22:56:57 +0600

    create a simple class

---
+++ simple_class.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <cstdio>
+#include <map>
+using namespace std;
+
+class Car{
+    private:
+        string name;
+        double price;
+        string color;
+};
+
+int main(){
+    Car car1 = Car();
+
+    return 0;
+}
