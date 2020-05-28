81e3d8d1e066501aa0f8237af6864a59142169e2
shahad-mahmud <shahad9381@gmail.com>
2020-05-28 23:00:15 +0600

    added simple methods

--- simple_class.cpp
+++ simple_class.cpp
@@ -10,10 +10,22 @@ class Car{
         string name;
         double price;
         string color;
+    public:
+        void set_name(string name){
+            this->name = name;
+        }
+
+        string get_name(){
+            return this->name;
+        }
 };
 
 int main(){
     Car car1 = Car();
+
+    car1.set_name("Audi");
+
+    cout << car1.get_name();
 
     return 0;
 }
